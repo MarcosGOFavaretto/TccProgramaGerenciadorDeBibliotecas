5b58be04264a8bcdae6c053e35c451a442d08b93
MarcosGOFavaretto <marcos.favaretto@etec.sp.gov.br>
2021-03-06 17:15:41 -0300

    :ok_hand:REFACTOR: Refatora a classe de Agendamentos!
    Refatora a classe de agendamentos representada pelo arquivo AgendamentoClass

--- TccProgramaGerenciadorDeBibliotecas/src/Controller/AgendamentoClass.java
+++ TccProgramaGerenciadorDeBibliotecas/src/Controller/AgendamentoClass.java
@@ -15,25 +15,25 @@ import java.sql.ResultSet;
  */
 public class AgendamentoClass {
 
-    private int id_agendamento;
-    private int rm_aluno;
-    private int id_livro;
+    private int codigoDoAgendamento;
+    private int registroDeMatriculaDoAluno;
+    private int codigoDoLivro;
     private int quantidade;
-    public ResultSet resultset_visualizaragendamento;
-    private CadastrarAgendamento cadastraragendamento_objeto = new CadastrarAgendamento();
-    private VisualizarAgendamento visualizaragendamento_objeto = new VisualizarAgendamento();
+    public ResultSet resultadosDaVisualizacaoDeAgendamentos;
+    private CadastrarAgendamento objetoDaClasseCadastrarAgendamento = new CadastrarAgendamento();
+    private VisualizarAgendamento objetoDaClasseVisualizarAgendamento = new VisualizarAgendamento();
 
     // CRIANDO OS MÉTODOS "GETTERS":
-    public int getId_agendamento() {
-        return id_agendamento;
+    public int getCodigoDoAgendamento() {
+        return codigoDoAgendamento;
     }
 
-    public int getRm_aluno() {
-        return rm_aluno;
+    public int getRegistroDeMatriculaDoAluno() {
+        return registroDeMatriculaDoAluno;
     }
 
-    public int getId_livro() {
-        return id_livro;
+    public int getCodigoDoLivro() {
+        return codigoDoLivro;
     }
 
     public int getQuantidade() {
@@ -41,16 +41,16 @@ public class AgendamentoClass {
     }
 
     // CRIANDO OS MÉTODOS "SETTERS":
-    public void setId_agendamento(int id_agendamento) {
-        this.id_agendamento = id_agendamento;
+    public void setCodigoDoAgendamento(int codigoDoAgendamento) {
+        this.codigoDoAgendamento = codigoDoAgendamento;
     }
 
-    public void setRm_aluno(int rm_aluno) {
-        this.rm_aluno = rm_aluno;
+    public void setRegistroDeMatriculaDoAluno(int registroDeMatriculaDoAluno) {
+        this.registroDeMatriculaDoAluno = registroDeMatriculaDoAluno;
     }
 
-    public void setId_livro(int id_livro) {
-        this.id_livro = id_livro;
+    public void setCodigoDoLivro(int codigoDoLivro) {
+        this.codigoDoLivro = codigoDoLivro;
     }
 
     public void setQuantidade(int quantidade) {
@@ -58,7 +58,7 @@ public class AgendamentoClass {
     }
 
     public boolean cadastrarAgendamento(int rm_aluno, int id_livro, int quantidade) {
-        if (cadastraragendamento_objeto.cadastrarAgendamento(rm_aluno, id_livro, quantidade)) {
+        if (objetoDaClasseCadastrarAgendamento.cadastrarAgendamento(rm_aluno, id_livro, quantidade)) {
             return true;
         } else {
             return false;
@@ -66,7 +66,7 @@ public class AgendamentoClass {
     }
 
     public ResultSet visualizarAgendamento() {
-        resultset_visualizaragendamento = visualizaragendamento_objeto.visualizarAgendamento();
-        return resultset_visualizaragendamento;
+        resultadosDaVisualizacaoDeAgendamentos = objetoDaClasseVisualizarAgendamento.visualizarAgendamento();
+        return resultadosDaVisualizacaoDeAgendamentos;
     }
 }
